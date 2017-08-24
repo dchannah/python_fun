d1dc124d450ac13c48626c28fa60ea52bc87e20b
Dan Hannah <dchannah@lbl.gov>
2017-08-23 22:39:11 -0700

    Updating to run at two times.

--- eastbaycrawler/scheduled_run.sh
+++ eastbaycrawler/scheduled_run.sh
@@ -1,5 +1,11 @@
 # An example of how to run this using just cron.daily and at.
 
 bot_script_location=~/python_fun/eastbaycrawler/run_bot.sh
-timediff="8 hours"
-at now +`echo ${timediff}` -f ${bot_script_location}
+
+# First run
+timediff1="5 hours"
+at now +`echo ${timediff1}` -f ${bot_script_location}
+
+# Second run
+timediff2="5 hours"
+at now +`echo ${timediff2}` -f ${bot_script_location}
